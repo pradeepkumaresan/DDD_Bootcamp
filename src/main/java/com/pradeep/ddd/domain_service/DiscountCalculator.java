fdd8fcdf11f907afa3ecc9556fa716987cbb7920
pradeepkumaresan <pradeepk@thoughtworks.com>
2023-01-20 15:17:37 +0530

    discount price

--- src/main/java/com/pradeep/ddd/domain_service/DiscountCalculator.java
+++ src/main/java/com/pradeep/ddd/domain_service/DiscountCalculator.java
@@ -1,8 +1,17 @@
 package com.pradeep.ddd.domain_service;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DiscountCalculator {
+
+    Map<String, BigDecimal> competitorPrices = new HashMap<>();
+
+    public DiscountCalculator() {
+        competitorPrices.put("Apple Pencil", new BigDecimal(75));
+        competitorPrices.put("Sony Wireless headphone", new BigDecimal(100));
+    }
 
     public static BigDecimal discountByTenPercent(BigDecimal amount){
         return amount.subtract(amount.multiply(new BigDecimal("0.1")));
